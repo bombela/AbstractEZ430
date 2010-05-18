440db2f4bb05ab796ab7679f2381676533708aed
bombela <bombela@gmail.com>
2010-05-17 18:08:58 -0700

    watch test

---
+++ tests/watch/test.cpp
@@ -0,0 +1,68 @@
+/*
+ * tests/ap/test.cpp
+ * Copyright © 2010 François-Xavier 'Bombela' Bourlet <bombela@gmail.com>
+ *
+*/
+
+#include <ez430_watch/ap.h>
+#include <ez430_watch/watch.h>
+
+#include <iostream>
+
+using namespace ez430;
+
+int main(int argc, char const* argv[])
+{
+	if (argc < 2)
+	{
+		std::cout << "usage: " << "/dev/ttyACM0 cmd" << std::endl;
+		return -1;
+	}
+
+	AccessPoint ap;
+
+	try
+	{
+		ap.open(argv[1]);
+	}
+	catch (...)
+	{
+	}
+
+	if (!ap.isOpen())
+	{
+		std::cout << "not open !" << std::endl;
+		return -1;
+	}
+
+	std::cout << std::hex << "product id: " << ap.getProductId() << std::dec << std::endl;
+	std::cout << "current radio state: " << ap.getRadioState() << std::endl;
+
+	Watch watch(ap.getService());
+
+	if (argc > 2)
+	{
+		std::string arg = argv[2];
+		if (arg == "start")
+		{
+			std::cout << "Starting radio..." << std::endl;
+			ap.startRadio();
+		}
+		else if (arg == "stop")
+		{
+			std::cout << "Stopping radio..." << std::endl;
+			ap.stopRadio();
+		}
+		else if (arg == "acc")
+		{
+			std::cout << "Retrieving acc..." << std::endl;
+			for (;;)
+			{
+				Motion motion = watch.getMotion();
+				std::cout << motion << std::endl;
+			}
+		}
+	}
+	std::cout << "bye..." << std::endl;
+	return 0;
+}
