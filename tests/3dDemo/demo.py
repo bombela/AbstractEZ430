fb250f72070366f33723594ce6cfe1657c6d6bb5
Jeffrey Macko <jeffrey@streamit.fr>
2010-05-18 11:16:52 -0700

    Revert "Adding sword model"
    This reverts commit 38d0b965aa7b4adf55eb8c490fb0be49cb2b268e.

--- tests/3dDemo/demo.py
+++ tests/3dDemo/demo.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
 
+
 #
 # TODO
+# - Refasse la classe Weapon
+# - Ajout possibiliter d'action spesifique avec bouton specifique
+# - Affichage de box qui log les informations clavier
+# - Bouton pour changer d'item
+
+# - Backtravelling Camera (reset position)
 # - Chargement lightsaber
+# - Reparation du fog
+# - Mise en place de support // Damier
+# - Virer le bug d'execution
 #
+
 
 import sys, os, os.path, soya, soya.sdlconst
 import soya.widget as widget
 import ez430
 import time
+import math
 from threading import Thread
 
 ap = ez430.AccessPoint()
@@ -26,6 +38,7 @@ if ap.isOpen():
 		ap.startRadio()
 
 watch = ez430.Watch(ap.getService())
+watch.setSmooth(0.40)
 
 class MotionThread(Thread):
 	def __init__ (self):
@@ -38,7 +51,7 @@ class MotionThread(Thread):
 			print "Access point not open... stop thread"
 			return
 		while self.running:
-			time.sleep(0.5)
+			time.sleep(0.1)
 			try:
 				self.motion = False
 				motion = watch.getMotion()
@@ -53,72 +66,74 @@ motionThread = MotionThread()
 motionThread.start()
 
 # Initialisation
-#soya.init(width = 1024, height = 768)
 soya.init()
 soya.path.append(os.path.join(os.path.dirname(sys.argv[0]), "data"))
 
 # Creates a scene.
 scene = soya.World()
 
-#Classe Weapon
-class Weapon(soya.Body):
-
-	# Initialisation Object (constructeur)
-	def __init__(self, parent, w_name):
-		soya.Body.__init__(self, parent, soya.Model.get(w_name))
+# Classe personaliser pour la gestion de l'epee
+class ArticulateSword(soya.Body):
+	def __init__(self, parent):
+#		soya.Body.__init__(self, parent, soya.Model.get("sword"))
+		soya.Body.__init__(self, parent, soya.Model.get("katana"))
 		self.speed = soya.Vector(self, 0.0, 0.0, 0.0)
-		self.rotation_y_speed = 0.
-		self.rotation_x_speed = 0.
-		self.rotation_z_speed = 0.
+		self.rotation_y_speed = 0.0
+		self.rotation_x_speed = 0.0
+		self.rotation_z_speed = 0.0
 		self.solid = 0
-		self.lastmotion = ez430.Motion()
-	# Gestion des events
-	def begin_round(self):
-		soya.Body.begin_round(self)
-		motion = motionThread.motion
-		if motion != False:
-			self.turn_x(motion.x * 2. * 0.2 + self.lastmotion.x * 0.1)
-			self.turn_y(motion.y * 2. * 0.2 + self.lastmotion.y * 0.1)
-			self.turn_z(motion.z * 2. * 0.2 + self.lastmotion.z * 0.1)
-			self.lastmotion = motion
-
         
 	def begin_round(self):
 		soya.Body.begin_round(self)
+		for event in soya.MAIN_LOOP.events:
+			if event[0] == soya.sdlconst.KEYDOWN:
+				if   event[1] == soya.sdlconst.K_UP:     self.rotation_y_speed = -10.0
+				elif event[1] == soya.sdlconst.K_DOWN:   self.rotation_y_speed = 10.0
+				elif event[1] == soya.sdlconst.K_LEFT:   self.rotation_x_speed = -10.0
+				elif event[1] == soya.sdlconst.K_RIGHT:  self.rotation_x_speed = 10.0
+				elif event[1] == soya.sdlconst.K_LSHIFT: self.reset_pos()
+			elif event[0] == soya.sdlconst.KEYUP:
+				if   event[1] == soya.sdlconst.K_UP:     self.rotation_y_speed = 0.0
+				elif event[1] == soya.sdlconst.K_DOWN:   self.rotation_y_speed = 0.0
+				elif event[1] == soya.sdlconst.K_LEFT:   self.rotation_x_speed = 0.0
+				elif event[1] == soya.sdlconst.K_RIGHT:  self.rotation_x_speed = 0.0
+				elif event[1] == ord('z'):
+					#self.rotate(90, scene, soya.Point(scene, 0.0, 1.0, 0.0))
+					self.turn_x(45)
+				elif event[1] == ord('q'):
+					self.turn_y(-45)
+
+			elif event[0] == soya.sdlconst.QUIT:
+				soya.MAIN_LOOP.stop()
+
 		motion = motionThread.motion
 		if motion != False:
-			self.turn_x(motion.x * 2. * 0.2 + self.lastmotion.x * 0.1)
-			self.turn_y(motion.y * 2. * 0.2 + self.lastmotion.y * 0.1)
-			self.turn_z(motion.z * 2. * 0.2 + self.lastmotion.z * 0.1)
-			self.lastmotion = motion
+			#self.turn_x(motion.x * 2.20)
+			if abs(motion.y) > 2:
+				self.turn_y(motion.y)
+			#self.turn_z(motion.z)
+
+		#self.rotate_x(self.rotation_x_speed)
+		#self.rotate_y(self.rotation_y_speed)
+		#self.rotate_z(self.rotation_z_speed)
 
 	def advance_time(self, proportion):
 		soya.Body.advance_time(self, proportion)
+		#self.add_mul_vector(proportion, self.speed)
 
-	# Affichage des donnees recues
+	def reset_pos(self):
+		print "Reset des position"
+
 	def aff_data(self):
-		print "Received Data -> button: x: y: z:"
-
-	# Fonction Button 1 Pressed
-	def funcion_1(self):
-		print "Fonction Button 1 Pressed"
-		print self.old_moves
-		self.old_moves.clear()
-	# Fonction Button 2 Pressed
-	def funcion_2(self):
-		print "Fonction Button 2 Pressed"
-
-	# Fonction Button 3 Pressed
-	def funcion_3(self):
-		print "Fonction Button 3 Pressed"
+		print "Affichage des donnees a chaque"
 
 # Construction Vrai Sword
-weapon = Weapon(scene, "sword")
+sword = ArticulateSword(scene)
 
 # Parametrage Sword
-weapon.x = 0.
-weapon.y = 0.
-weapon.z = 20.
+sword.x = 0.
+sword.y = 0.
+sword.z = 25.
 #sword.rotate_y(90.0)
 #sword.rotate_z(-90.0)
 
@@ -149,7 +164,17 @@ camera.x = 0.
 camera.y = 0.
 camera.z = 5.
 
-camera.look_at(weapon)
+#traveling = soya.ThirdPersonTraveling(sword)
+#traveling.distance = 15.0
+
+#camera.add_traveling(traveling)
+#camera.speed = 0.3
+#camera.x = -5.
+#camera.y = -5.
+#camera.z = -5.
+
+
+camera.look_at(sword)
 
 # Application de la camera a la l'application
 soya.set_root_widget(camera)
