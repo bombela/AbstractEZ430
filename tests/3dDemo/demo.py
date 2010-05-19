2ff71e3e6da6c2d1dfc1789c815dae228330ca5c
Jeffrey Macko <jeffrey@streamit.fr>
2010-05-19 16:46:14 -0700

    Test modification demo

--- tests/3dDemo/demo.py
+++ tests/3dDemo/demo.py
@@ -28,7 +28,7 @@ if ap.isOpen():
 		ap.startRadio()
 
 watch = ez430.Watch(ap.getService())
-watch.setSmooth(0.40)
+watch.setSmooth(0.80)
 
 class MotionThread(Thread):
 	def __init__ (self):
@@ -63,6 +63,7 @@ soya.path.append(os.path.join(os.path.dirname(sys.argv[0]), "data"))
 # Creates a scene.
 scene = soya.World()
 
+
 #Classe Weapon
 class Weapon(soya.Body):
 
@@ -70,35 +71,84 @@ class Weapon(soya.Body):
 	def __init__(self, parent, w_name):
 		soya.Body.__init__(self, parent, soya.Model.get(w_name))
 		self.speed = soya.Vector(parent, 0.0, 0.0, 0.0)
+		self.p_speed = soya.Vector(parent, 0.,0.,0.) # previous speed
+
+		self.r_speed = soya.Vector(parent, 0.,0.,0.) # rotation speed
+		self.p_r_speed = soya.Vector(parent, 0.,0.,0.) # previous rotation speed
 		self.solid = 0
 		self.lastmotion = ez430.Motion()
-		self.rotation_speed = 0
+		self.rotation_x_speed = 0
+		self.rotation_y_speed = 0
+		self.rotation_z_speed = 0
 		self.a = 0 # angle
 		self.motion = motionThread.motion
 
+	# Calcule de la difference entre ancien et nouveaux
+	def difference(self):
+		def __init__(self):
+			self.diff = False
+		def calc(self, old, new):
+			
+
 	# Gestion des events
 	def begin_round(self):
-		soya.Body.begin_round(self)		
-		self.motion = motion = motionThread.motion
-		if motion != False:
-			h = math.hypot(motion.x, motion.y) # hypotenuse
-			if h != 0:
-				self.a = math.degrees(math.asin(motion.y / h))
-			else:
-				self.a = 90
-			print " x:%d" %motion.x
-			print " y:%d" %motion.y
-			print " z:%d" %motion.z
-			print " h:%d" %h
-			print " a:%d" %self.a
-			self.speed.x = motion.x
-			self.speed.y = motion.y
-			self.speed.z = motion.z
+		soya.Body.begin_round(self)
+		self.speed.x = 0.
+		self.speed.x = 0.
+		self.speed.x = 0.
+
+		for event in soya.MAIN_LOOP.events:
+			if event[0] == soya.sdlconst.KEYDOWN:
+				# Translation
+				if   event[1] == soya.sdlconst.K_UP:     self.speed.z =  0.1
+				elif event[1] == soya.sdlconst.K_DOWN:   self.speed.z =  -0.2
+				elif event[1] == soya.sdlconst.K_LEFT:   self.speed.x = 0.1
+				elif event[1] == soya.sdlconst.K_RIGHT:  self.speed.x = -0.2
+				elif event[1] == soya.sdlconst.K_w:   self.speed.y = 0.1
+				elif event[1] == soya.sdlconst.K_s:  self.speed.y = -0.2
+
+				# Rotation
+				elif event[1] == soya.sdlconst.K_a:   self.r_speed.y = -10.0
+				elif event[1] == soya.sdlconst.K_d:  self.r_speed.y =  10.0
+				elif event[1] == soya.sdlconst.K_q:   self.r_speed.x = -10.0
+				elif event[1] == soya.sdlconst.K_e:  self.r_speed.x =  10.0
+				elif event[1] == soya.sdlconst.K_z:   self.r_speed.z = -10.0
+				elif event[1] == soya.sdlconst.K_c:  self.r_speed.z =  10.0
+
+#		self.previous = soya.Vector(parent, self.speed.x, self.speed.y, self.speed.z)
+#		self.motion = motion = motionThread.motion
+#		if motion != False:		
+#			h = math.hypot(motion.x, motion.y) # hypotenuse
+#			if h != 0:
+#				self.a = math.degrees(math.asin(motion.y / h))
+#			else:
+#				self.a = 90
+#			print " x:%d" %motion.x
+#			print " y:%d" %motion.y
+#			print " z:%d" %motion.z
+#			print " h:%d" %h
+#			print " a:%d" %self.a
+
+
+#			self.speed.x = 0
+#			self.speed.y = 0		
+#			self.speed.z = (motion.z * -1) / 100
+#			self.add_vector(self.speed)
+
+#			self.speed.x = 0
+#			self.speed.y = motion.x / 100
+#			self.speed.z = 0
+#			self.add_vector(self.speed)
+
 #			self.rotation_speed = random.uniform(-25., 25.)
-			self.rotate_axis(self.a, self.speed)
+#			self.rotate_axis(self.a, self.speed)
 
 	def advance_time(self, proportion):
 		soya.Body.advance_time(self, proportion)
+		self.add_vector(self.speed)
+		self.rotate_x(self.rotation_x_speed)
+		self.rotate_y(self.rotation_y_speed)
+		self.rotate_z(self.rotation_z_speed)
 #		if self.motion != False:
 #			self.add_mul_vector(proportion, self.speed)
 
