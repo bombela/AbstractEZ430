47daf63fc2b7b6748f7054cd2b5e8ef16dd18773
bombela <bombela@gmail.com>
2010-05-18 17:30:11 -0700

    binding functionnal need a real python package

--- binding/python/ap.cpp
+++ binding/python/ap.cpp
@@ -4,10 +4,13 @@
  *
 */
 
+#include <sstream>
+
 #include <boost/python.hpp>
 
 #include <ez430_watch/ap.h>
 #include <ez430_watch/service.h>
+#include <ez430_watch/watch.h>
 
 using namespace boost::python;
 using namespace ez430;
@@ -19,21 +22,24 @@ struct ServiceWrap: Service, wrapper<Service>
 	virtual bool getSyncData(SyncData& d) { return get_override("getSyncData")(d); };
 	virtual bool setSyncData(const SyncData& d) { return get_override("setSyncData")(d); };
 	virtual bool exitWatchSync() { return get_override("exitWatchSync"); };
+
+	operator Service&() { return *this; }
 };
 
 BOOST_PYTHON_MODULE(ez430)
 {
 	class_<ServiceWrap, boost::noncopyable>("Service")
+		.def("getMotion", &ServiceWrap::getMotion)
 		;
 
 	enum_<AccessPoint::RadioState>("RadioState")
 		.value("STOPPED", AccessPoint::STOPPED)
 		.value("LINKING", AccessPoint::LINKING)
 		.value("LINKED", AccessPoint::LINKED)
-	;
+		;
 
 	class_<AccessPoint>("AccessPoint")
-		.def(init<std::string>())
+		.def(init<const std::string&>())
 		.def("open", &AccessPoint::open)
 		.def("isOpen", &AccessPoint::isOpen)
 		.def("close", &AccessPoint::close)
@@ -41,6 +47,26 @@ BOOST_PYTHON_MODULE(ez430)
 		.def("startRadio", &AccessPoint::startRadio)
 		.def("stopRadio", &AccessPoint::stopRadio)
 		.def("getRadioState", &AccessPoint::getRadioState)
-	//	.def("getService", &AccessPoint::getService, return_internal_reference<1>())
-	;
+		.def("getService", &AccessPoint::getService, return_internal_reference<1>())
+		;
+
+	class_<Watch>("Watch", init<Service&>())
+		.def("getMotion", &Watch::getMotion)
+		.def("getButton", &Watch::getButton)
+		;
+
+	enum_<Motion::Button>("Button")
+		.value("NONE", Motion::NONE)
+		.value("STAR", Motion::STAR)
+		.value("NUM", Motion::NUM)
+		.value("UP", Motion::UP)
+		;
+
+	class_<Motion>("Motion", no_init)
+		.def_readwrite("button", &Motion::button)
+		.def_readwrite("haveAcc", &Motion::haveAcc)
+		.def_readwrite("x", &Motion::x)
+		.def_readwrite("y", &Motion::y)
+		.def_readwrite("z", &Motion::z)
+		;
 }
--- tests/pywatch/pywatch.py
+++ tests/pywatch/pywatch.py
@@ -8,6 +8,21 @@ ap.open("/dev/ttyACM0")
 
 print "%x" % (ap.getProductId())
 
-print ap.getRadioState()
+print "Current radio state: " + str(ap.getRadioState())
 
-ap.getService()
+if ap.getRadioState() == ez430.RadioState.STOPPED:
+	print "Starting radio..."
+	ap.startRadio()
+
+watch = ez430.Watch(ap.getService())
+
+try:
+	while 1:
+		try:
+			while 1:
+				motion = watch.getMotion()
+				print motion
+		except RuntimeError:
+			print "Link timeout... retry !"
+except (KeyboardInterrupt, SystemExit):
+	print "loez"
