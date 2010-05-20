adf1a5ea859eb6f486da8b5e39fa7e8ede8376cc
Jeffrey Macko <jeffrey@streamit.fr>
2010-05-19 22:35:44 -0700

    Nettoyage mais tjr pas fonctionnel

--- tests/3dDemo/demo.py
+++ tests/3dDemo/demo.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 
-#
-# TODO
-# - Chargement lightsaber
-#
-
+# Soya & Python
 import sys, os, os.path, soya, soya.sdlconst
 import soya.widget as widget
+import math
+import random
+
+# For the watch works
 import ez430
 import time
 from threading import Thread
-import math
-import random
 
 ap = ez430.AccessPoint()
 
@@ -56,8 +54,7 @@ motionThread = MotionThread()
 motionThread.start()
 
 # Initialisation
-#soya.init(width = 1024, height = 768)
-soya.init()
+soya.init(width = 1024, height = 768)
 soya.path.append(os.path.join(os.path.dirname(sys.argv[0]), "data"))
 
 # Creates a scene.
@@ -71,31 +68,34 @@ class Weapon(soya.Body):
 	def __init__(self, parent, w_name):
 		soya.Body.__init__(self, parent, soya.Model.get(w_name))
 		self.speed = soya.Vector(parent, 0.0, 0.0, 0.0)
-		self.p_speed = soya.Vector(parent, 0.,0.,0.) # previous speed
+#		self.p_speed = soya.Vector(parent, 0.,0.,0.) # previous speed
 
 		self.r_speed = soya.Vector(parent, 0.,0.,0.) # rotation speed
-		self.p_r_speed = soya.Vector(parent, 0.,0.,0.) # previous rotation speed
+		self.p_accel = soya.Vector(parent, 0.,0.,0.) # pourcentage accel speed
+		self.p_speed = soya.Vector(parent, 0.,0.,0.) # pourcentage accel speed
+#		self.p_r_speed = soya.Vector(parent, 0.,0.,0.) # previous rotation speed
 		self.solid = 0
 		self.lastmotion = ez430.Motion()
-		self.rotation_x_speed = 0
-		self.rotation_y_speed = 0
-		self.rotation_z_speed = 0
 		self.a = 0 # angle
-		self.motion = motionThread.motion
-
-	# Calcule de la difference entre ancien et nouveaux
-	def difference(self):
-		def __init__(self):
-			self.diff = False
-		def calc(self, old, new):
-			
+		self.b = 0
 
 	# Gestion des events
 	def begin_round(self):
 		soya.Body.begin_round(self)
+
 		self.speed.x = 0.
-		self.speed.x = 0.
-		self.speed.x = 0.
+		self.speed.y = 0.
+		self.speed.z = 0.
+
+		self.r_speed.x = 0.
+		self.r_speed.y = 0.
+		self.r_speed.z = 0.
+
+		self.p_accel.x = 0.
+		self.p_accel.x = 0.
+		self.p_accel.x = 0.
+
+		self.a = 0 # angle
 
 		for event in soya.MAIN_LOOP.events:
 			if event[0] == soya.sdlconst.KEYDOWN:
@@ -115,40 +115,41 @@ class Weapon(soya.Body):
 				elif event[1] == soya.sdlconst.K_z:   self.r_speed.z = -10.0
 				elif event[1] == soya.sdlconst.K_c:  self.r_speed.z =  10.0
 
-#		self.previous = soya.Vector(parent, self.speed.x, self.speed.y, self.speed.z)
-#		self.motion = motion = motionThread.motion
-#		if motion != False:		
-#			h = math.hypot(motion.x, motion.y) # hypotenuse
-#			if h != 0:
-#				self.a = math.degrees(math.asin(motion.y / h))
-#			else:
-#				self.a = 90
-#			print " x:%d" %motion.x
-#			print " y:%d" %motion.y
-#			print " z:%d" %motion.z
-#			print " h:%d" %h
-#			print " a:%d" %self.a
+		motion = motionThread.motion
+		if motion != False:
+			h = math.hypot(motion.x, motion.y) # hypotenuse
+			if h != 0:
+				self.a = math.degrees(math.asin(motion.y / h))
+			print " x:%d" %motion.x
+			print " y:%d" %motion.y
+			print " z:%d" %motion.z
+			print " h:%d" %h
+			print " a:%d" %self.a
 
+			
 
 #			self.speed.x = 0
-#			self.speed.y = 0		
-#			self.speed.z = (motion.z * -1) / 100
+#			self.speed.y = 0
+#			self.speed.z = motion.z * -1
 #			self.add_vector(self.speed)
 
-#			self.speed.x = 0
-#			self.speed.y = motion.x / 100
+#			self.speed.x = motion.x
+#			self.speed.y = 0
 #			self.speed.z = 0
 #			self.add_vector(self.speed)
 
+#			self.b = self.b + 1
 #			self.rotation_speed = random.uniform(-25., 25.)
 #			self.rotate_axis(self.a, self.speed)
-
+#			if self.b > 5:
+#				print do + some + shit
 	def advance_time(self, proportion):
 		soya.Body.advance_time(self, proportion)
 		self.add_vector(self.speed)
-		self.rotate_x(self.rotation_x_speed)
-		self.rotate_y(self.rotation_y_speed)
-		self.rotate_z(self.rotation_z_speed)
+#		self.rotate_axis(self.a, self.speed)
+#		self.rotate_x(self.r_speed.x)
+#		self.rotate_y(self.r_speed.y)
+#		self.rotate_z(self.r_speed.z)
 #		if self.motion != False:
 #			self.add_mul_vector(proportion, self.speed)
 
@@ -167,21 +168,14 @@ class Weapon(soya.Body):
 	# Fonction Button 3 Pressed
 	def funcion_3(self):
 		print "Fonction Button 3 Pressed"
-	
-	def init_xyz(self, x, y,z):
-		self.before_x = x
-		self.before_y = y
-		self.before_z = z
 
 # Construction Vrai Sword
-weapon = Weapon(scene, "katana")
+weapon = Weapon(scene, "sword")
 
 # Parametrage Sword
 weapon.x = 0.
 weapon.y = 0.
 weapon.z = 20.
-#sword.rotate_y(90.0)
-#sword.rotate_z(-90.0)
 
 # Gestion de la lumiere
 light = soya.Light(scene)
