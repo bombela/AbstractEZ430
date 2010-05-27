5a8327f01da7303ec97e97948a964246f0b34602
Macko Jeffrey <mak972@gmail.com>
2010-05-26 19:55:04 -0700

    Ajout du fichier doxyfile

--- include/ez430_watch/ap.h
+++ include/ez430_watch/ap.h
@@ -13,33 +13,95 @@
 #include <string>
 #include <vector>
 
+
+/**
+* @namespace ez430
+* Global namespace of the library
+*/
 namespace ez430 {
 
+/**
+* @class AccessPoint
+* Specific namespace of the AccessPoint
+*/
 class AccessPoint
 {
 	public:
-		enum RadioState { STOPPED, LINKING, LINKED };
+        /**
+        * @enum RadioState
+        * Represents the different states of the Access Point
+        */
+		enum RadioState {
+		    STOPPED, /**<The Access Point is Stopped */
+		    LINKING, /**<The Access Point is in Linking */
+		    LINKED /**<The Access Point is Linked */
+		};
 
 		~AccessPoint();
 		AccessPoint();
+
+        /**
+        * @brief Constructor
+        * @param line : Is the serial port name
+        */
 		AccessPoint(const std::string& line);
 
+        /**
+        * @brief Open a serial port
+        * @param line : path of the serial port
+        */
 		void open(const std::string& line);
+
+        /**
+        * @brief Test if the serial port is open
+        * @return True if the serial port is open, false otherwise
+        */
 		bool isOpen() const;
+
+        /**
+        * @brief Close the Access Point serial port
+        */
 		void close();
 
+        /**
+        * @brief Get the product id
+        * @return Return the product id
+        */
 		int getProductId() const;
 
+        /**
+        * @brief Start the Access Point radio
+        */
 		void startRadio();
+
+        /**
+        * @brief Stop the Access Point radio
+        */
 		void stopRadio();
 		
+        /**
+        * @brief Get the status of the Access Point
+        * @return
+        */
 		RadioState getRadioState();
 
+        /**
+        * @brief Get the Access Point service
+        * @return 
+        */
 		protocol::Service& getService();
 
+        /**
+        * @brief Get the serial port list of Access Point connected
+        * @return List of relieable Access Point
+        */
 		static std::vector<std::string> probePorts();
 
 	private:
+        /**
+        * @class Implementation
+        * PIMPL Idiom
+        */
 		class Implementation *_impl;
 };
 
--- include/ez430_watch/service.h
+++ include/ez430_watch/service.h
@@ -8,9 +8,22 @@
 #ifndef SERVICE_H
 #define SERVICE_H
 
+/**
+* @namespace ez430
+* Global namespace of the library
+*/
 namespace ez430 {
+
+/**
+* @namespace protocol
+* Global namespace for the protocole
+*/
 namespace protocol {
 
+/**
+* @struct SyncData
+* Representation of the data in the watch
+*/
 struct SyncData
 {
 	bool  useMetric;
@@ -26,9 +39,22 @@ struct SyncData
 	int   altitude;
 };
 
+/**
+* @struct MotionData
+* Contains all information the watch send per each packet of ACC or PPT
+*/
 struct MotionData
 {
-	enum Button { NONE, STAR, NUM, UP };
+    /**
+    * @enum Button
+    * Represent the state of the Button on the watch
+    */
+	enum Button {
+	    NONE, /**<Means no button have been pressed in this frame */
+	    STAR, /**<Means star (*) button have been pressed in this frame */
+	    NUM, /**<Means num (#) button have been pressed in this frame */
+	    UP /**<Means up (^) button have been pressed in this frame */
+	};
 
 	Button button;
 
@@ -38,16 +64,43 @@ struct MotionData
 	int    z;
 };
 
+/**
+* @class Service
+* This class can manipulate data on watch easier
+*/
 class Service
 {
 	public:
 		virtual ~Service() {};
 
+        /**
+        * @brief Get the Motion from the connected device
+	    * @return If True getMotion succeed otherwise return false
+        */
 		virtual bool     getMotion(MotionData& d)       = 0;
 		
+        /**
+        * @brief Ask Synchonisation Data from the watch
+	    * @return If True requestSyncData succeed otherwise return false
+        */
 		virtual bool     requestSyncData()              = 0;
+
+        /**
+        * @brief Get the Synchonisation Data
+	    * @return If True getSyncData succeed otherwise return false
+        */
 		virtual bool     getSyncData(SyncData& d)       = 0;
+
+        /**
+        * @brief Set the synchronisation on the watch
+	    * @return If True setSyncData succeed otherwise return false
+        */
 		virtual bool     setSyncData(const SyncData& d) = 0;
+
+		/**
+	    * @brief Exit the sync mode on the watch
+	    * @return If True exit succeed otherwise return false
+	    */
 		virtual bool     exitWatchSyncMode()            = 0;
 };
 
